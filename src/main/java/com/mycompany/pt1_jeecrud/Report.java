9d1cf7388e28e9ef0cfadeb125bba756a65b92c0
SBSonk <sg.buenaventura04@gmail.com>
2023-12-13 01:52:13 +0800

    REPORTS

--- src/main/java/com/mycompany/pt1_jeecrud/Report.java
+++ src/main/java/com/mycompany/pt1_jeecrud/Report.java
@@ -4,20 +4,25 @@
  */
 package com.mycompany.pt1_jeecrud;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.properties.UnitValue;
 import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
-import org.xhtmlrenderer.pdf.ITextRenderer;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -25,8 +30,6 @@ import org.xhtmlrenderer.pdf.ITextRenderer;
  */
 @ManagedBean(name = "Reports", eager = true)
 public class Report {
-    final String FILEPATH = "/reports/";
-    
     java.util.Date reportDateFrom;
     java.util.Date reportDateTo;
 
@@ -81,26 +84,134 @@ public class Report {
         return transactionData;
     }
     
-    public void GenerateReport() {
-        ArrayList<Transaction> transactionsBetweenDate = GetTransactionsBetweenDate(reportDateFrom, reportDateTo);
-        
-        String path = FILEPATH + "report-" + System.currentTimeMillis() + ".pdf";
-        
-        String xhtmlContent = "<html><body><h1>Hello, World!</h1></body></html>";
-        
-        File file = new File(path);
-        file.mkdirs();
-        
-        try (OutputStream os = new FileOutputStream(path)) {
-            ITextRenderer renderer = new ITextRenderer();
-            renderer.setDocumentFromString(xhtmlContent);
-            renderer.createPDF(os);
-        } catch (Exception e) {
-            System.out.println(e.getClass().getSimpleName());
+     public ArrayList GetAll() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException
+    {
+        ArrayList<Transaction> transactionData = new ArrayList();
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/program_db?zeroDateTimeBehavior=CONVERT_TO_NULL",
+                    "root",
+                    "");
+
+            Statement stmt = con.createStatement();
+            ResultSet results = stmt.executeQuery("SELECT transaction_id, product_id, user_id, transaction_type_id, date FROM tbl_transaction;");
+
+            while(results.next())
+            {
+                Transaction temp = new Transaction();
+
+                temp.setTransactionId(results.getInt("transaction_id"));
+                temp.setProductId(results.getInt("product_id"));
+                temp.setUserId(results.getInt("user_id"));
+                temp.setTransactionTypeId(results.getInt("transaction_type_id"));
+                temp.setDate(results.getDate("date"));
+
+                transactionData.add(temp);
+            }
+        } catch (SQLException | ClassNotFoundException | InstantiationError | IllegalAccessError e) {
+            e.printStackTrace();
         }
-        
-        
-        // todo: make pdf file
-        // todo: pdf save as
+
+        return transactionData;
+    }
+    
+     public void GenerateReportBetween(java.util.Date from, java.util.Date to) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+
+        // Set content type and headers for PDF download
+        externalContext.responseReset();
+        externalContext.setResponseContentType("application/pdf");
+        externalContext.setResponseHeader("Content-Disposition", "attachment; filename=\"TRANSACTION_REPORT.pdf\"");
+
+        try (OutputStream outputStream = externalContext.getResponseOutputStream()) {
+            PdfWriter writer = new PdfWriter(outputStream);
+            PdfDocument pdfDoc = new PdfDocument(writer);
+            Document doc = new Document(pdfDoc);
+
+            doc.add(new Paragraph("DATED TRANSACTION REPORT | " + LocalDate.now()));
+            doc.add(new Paragraph("From " + from.toString() + " to " + to.toString()));
+
+            Table transactionsTable = new Table(UnitValue.createPercentArray(new float[] {10, 10, 20, 10, 30, 10, 10}));
+            transactionsTable.setMarginTop(5);
+            transactionsTable.addCell("ID");
+            transactionsTable.addCell("Product ID");
+            transactionsTable.addCell("Product Name");
+            transactionsTable.addCell("Customer ID");
+            transactionsTable.addCell("Customer Name");
+            transactionsTable.addCell("Transaction Type");
+            transactionsTable.addCell("Date");
+
+            ArrayList<Transaction> transactionsBetweenDate = GetTransactionsBetweenDate(from, to);
+            for(Transaction t : transactionsBetweenDate) {
+                transactionsTable.addCell(String.valueOf(t.getTransactionId()));
+                transactionsTable.addCell(String.valueOf(t.getProductId()));
+                transactionsTable.addCell(t.getProductName(t.productId));
+                transactionsTable.addCell(String.valueOf(t.getUserId()));
+                transactionsTable.addCell(t.getCustomerName(t.userId));
+                transactionsTable.addCell(t.getTransactionTypeString(t.transactionTypeId));
+                transactionsTable.addCell(t.getDate().toString());
+            }
+            
+            doc.add(transactionsTable);
+
+            doc.close();
+            outputStream.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        facesContext.responseComplete();
+    }
+     
+    public void GenerateReportAll() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+
+        // Set content type and headers for PDF download
+        externalContext.responseReset();
+        externalContext.setResponseContentType("application/pdf");
+        externalContext.setResponseHeader("Content-Disposition", "attachment; filename=\"TRANSACTION_REPORT.pdf\"");
+
+        try (OutputStream outputStream = externalContext.getResponseOutputStream()) {
+            PdfWriter writer = new PdfWriter(outputStream);
+            PdfDocument pdfDoc = new PdfDocument(writer);
+            Document doc = new Document(pdfDoc);
+
+            doc.add(new Paragraph("TRANSACTION REPORT | " + LocalDate.now()));
+
+            Table transactionsTable = new Table(UnitValue.createPercentArray(new float[] {10, 10, 20, 10, 30, 10, 10}));
+            transactionsTable.setMarginTop(5);
+            transactionsTable.addCell("ID");
+            transactionsTable.addCell("Product ID");
+            transactionsTable.addCell("Product Name");
+            transactionsTable.addCell("Customer ID");
+            transactionsTable.addCell("Customer Name");
+            transactionsTable.addCell("Transaction Type");
+            transactionsTable.addCell("Date");
+
+            ArrayList<Transaction> transactionsBetweenDate = GetAll();
+            for(Transaction t : transactionsBetweenDate) {
+                transactionsTable.addCell(String.valueOf(t.getTransactionId()));
+                transactionsTable.addCell(String.valueOf(t.getProductId()));
+                transactionsTable.addCell(t.getProductName(t.productId));
+                transactionsTable.addCell(String.valueOf(t.getUserId()));
+                transactionsTable.addCell(t.getCustomerName(t.userId));
+                transactionsTable.addCell(t.getTransactionTypeString(t.transactionTypeId));
+                transactionsTable.addCell(t.getDate().toString());
+            }
+            
+            doc.add(transactionsTable);
+
+            doc.close();
+            outputStream.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        facesContext.responseComplete();
     }
 }
